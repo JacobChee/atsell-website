f8130fbbfcc1b69442a90b20d410340a2ea327a0
JacobChee <jacob@atsell.com>
2026-04-08 15:30:53 +0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AtsellLanding from './AtsellLanding';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AtsellLanding />
+  </React.StrictMode>
+);
